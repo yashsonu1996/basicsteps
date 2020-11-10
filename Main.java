3c5d6b21eedc036656d939b34cb779f3327571b7
yashsonu1996 <74137604+yashsonu1996@users.noreply.github.com>
2020-11-10 10:08:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class InsertionSort {
     }  
        
     public static void main(String a[]){    
-        int[] arr1 = {9,14,3,2,43,11,58,22};    
+        int[] arr1 = {9,14,11,58,22};    
         System.out.println("Before Insertion Sort");    
         for(int i:arr1){    
             System.out.print(i+" ");    
